1bbc51cff82e6a509abf8c609b551c0bf6105684
Harish mahur <hmahur20@gmail.com>
2022-08-08 22:26:35 +0530

    modified:dequeue

--- Queue.java
+++ Queue.java
@@ -1,17 +1,34 @@
 package com.stack_Queue_operations;
 
+import com.bridgelabz.linkedlist.LinkedList;
+
 public class Queue<T> {
-	LinkedList<T> linkedList;
+    LinkedList<T> linkedList;
 
-	public Queue() {
-		this.linkedList = new LinkedList<>();
-	}
+    public Queue() {
+        this.linkedList = new LinkedList<>();
+    }
 
-	public void enqueu(T key) {
-		linkedList.add(key);
-	}
+    public void enqueu(T key) {
+        linkedList.add(key);
+    }
+    public void print() {
+        linkedList.display();
+    }
 
-	public void print() {
-		linkedList.display();
-	}
+    public boolean isEmpty(){
+        if(linkedList.size() == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public T peek(){
+        return linkedList.peek();
+    }
+
+
+    public T dequeue() {
+        return linkedList.pop();
+    }
 }
