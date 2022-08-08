3f4ed32705e063dab61c4641d7b79cff0f0756e6
Harish mahur <hmahur20@gmail.com>
2022-08-08 21:56:08 +0530

    modified:push in stack

--- Node.java
+++ Node.java
@@ -1,5 +1,11 @@
 package com.stack_Queue_operations;
 
-public class Node {
+public class Node<T> {
 
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+    }
 }
